422f077d6d5c6f4b7f8f78a83ef085fe958a93f0
fabiogattofg-art <fabio.gatto.fg@gmail.com>
2026-06-12 00:52:36 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE='fg-v3';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k))))));
